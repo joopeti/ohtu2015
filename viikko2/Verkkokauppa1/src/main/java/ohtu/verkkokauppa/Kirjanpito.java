9d0df7d70c6b980671a7c95c33961d7d9081cdce
Pyry Joona <pyry.joona@helsinki.fi>
2015-03-21 17:23:37 +0200

    beans

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -0,0 +1,21 @@
+
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public class Kirjanpito {
+
+    private static ArrayList<String> tapahtumat;
+
+    public Kirjanpito() {
+        tapahtumat = new ArrayList<String>();
+    }
+
+    public void lisaaTapahtuma(String tapahtuma) {
+        tapahtumat.add(tapahtuma);
+    }
+
+    public static ArrayList<String> getTapahtumat() {
+        return tapahtumat;
+    }
+}
